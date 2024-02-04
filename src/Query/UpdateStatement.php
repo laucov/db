0e6f7878b8d90377af5083f32c17137c62e53fd4
covaleski <covaleski23@gmail.com>
2024-02-04 15:25:26 -0300

    feat: create `UpdateStatement`

---
+++ src/Query/UpdateStatement.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * This file is part of Laucov's Database Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package db
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+namespace Laucov\Db\Query;
+
+use Laucov\Db\Query\Traits\JoinClauseStatementTrait;
+
+/**
+ * Provides an interface to build a SQL SELECT query.
+ */
+class UpdateStatement implements \Stringable
+{
+    use JoinClauseStatementTrait;
+
+    /**
+     * Values to update.
+     * 
+     * @var array<string, string>
+     */
+    protected array $values = [];
+
+    /**
+     * Create the UPDATE statement instance.
+     */
+    public function __construct(
+        /**
+         * Table to update.
+         */
+        protected string $tableName,
+    ) {}
+
+    /**
+     * Get the UPDATE statement string representation.
+     */
+    public function __toString(): string
+    {
+        // Initialize statement.
+        $statement = "UPDATE {$this->tableName}";
+
+        // Add updated values.
+        if (count($this->values) > 0) {
+            $values = array_map(
+                fn (string $v, string $k) => "{$k} = {$v}",
+                $this->values,
+                array_keys($this->values),
+            );
+            $statement .= "\nSET " . implode(', ', $values);
+        }
+
+        // Add FROM clause.
+        $from = $this->compileFromClause();
+        if ($from !== null) {
+            $statement .= "\n{$from}";
+        }
+
+        // Add other clauses.
+        if (count($this->joinClauses) > 0) {
+            $statement .= "\n" . implode("\n", $this->joinClauses);
+        }
+        if ($this->whereClause !== null) {
+            $statement .= "\n{$this->whereClause}";
+        }
+
+        return $statement;
+    }
+
+    /**
+     * Set a value to update.
+     */
+    public function setValue(string $column, string $expression): static
+    {
+        $this->values[$column] = $expression;
+        return $this;
+    }
+}
---
+++ tests/Query/UpdateStatementTest.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * This file is part of Laucov's Database Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package db
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Query;
+
+use Laucov\Db\Query\JoinClause;
+use Laucov\Db\Query\UpdateStatement;
+use Laucov\Db\Query\WhereClause;
+use PHPUnit\Framework\TestCase;
+ 
+/**
+ * @coversDefaultClass \Laucov\Db\Query\UpdateStatement
+ */
+class UpdateStatementTest extends TestCase
+{
+    /**
+     * @covers ::__construct
+     * @covers ::__toString
+     * @covers ::compileFromClause
+     * @covers ::setFromClause
+     * @covers ::setValue
+     * @covers ::setWhereClause
+     * @uses Laucov\Db\Query\AbstractConditionalClause::addConstraint
+     * @uses Laucov\Db\Query\Constraint::__construct
+     * @uses Laucov\Db\Query\Constraint::__toString
+     * @uses Laucov\Db\Query\JoinClause::__toString
+     * @uses Laucov\Db\Query\JoinClause::setOn
+     * @uses Laucov\Db\Query\Traits\JoinClauseStatementTrait::addJoinClause
+     * @uses Laucov\Db\Query\WhereClause::__toString
+     */
+    public function testCanBuildAQuery(): void
+    {
+        // Test a simple query.
+        $expected_a = <<<SQL
+            UPDATE messages
+            SET read_at = '2024-03-04 14:48:32'
+            SQL;
+        
+        // Build.
+        $actual_a = (string) (new UpdateStatement('messages'))
+            ->setValue('read_at', "'2024-03-04 14:48:32'");
+
+        // Compare.
+        $this->assertSame($expected_a, $actual_a);
+
+        // Test a complex query.
+        $expected_b = <<<SQL
+            UPDATE orders
+            SET employee_id = NULL, canceled_at = '2024-02-04 13:19'
+            FROM orders AS o
+            INNER JOIN employees
+            ON employees.id = o.employee_id
+            WHERE employees.is_active != 1
+            SQL;
+        
+        // Build.
+        $actual_b = (string) (new UpdateStatement('orders'))
+            ->setValue('employee_id', 'NULL')
+            ->setValue('canceled_at', "'2024-02-04 13:19'")
+            ->setFromClause('orders', 'o')
+            ->addJoinClause(function (JoinClause $clause): void {
+                $clause
+                    ->setOn('INNER', 'employees')
+                    ->addConstraint('employees.id', '=', 'o.employee_id');
+            })
+            ->setWhereClause(function (WhereClause $clause): void {
+                $clause->addConstraint('employees.is_active', '!=', 1);
+            });
+        
+        // Compare.
+        $this->assertSame($expected_b, $actual_b);
+    }
+}
